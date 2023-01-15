e31851c40e72839fd7536d18acfc62bf1b5ffc27
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-01-14 23:04:52 -0600

    updates

--- 0x00-caching/0-basic_cache.py
+++ 0x00-caching/0-basic_cache.py
@@ -6,18 +6,18 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-  ''' Uses put and get functions '''
+    ''' Uses put and get functions '''
 
-  def put(self, key, item):
-    ''' Adds item to the cache '''
-    if key and item:
-      self.cache_data[key] = item
+    def put(self, key, item):
+        ''' Adds item to the cache '''
+        if key and item:
+            self.cache_data[key] = item
 
-  def get(self, key):
-    ''' Returns a value from the cache by key '''
-    if key:
-      return self.cache_data.get(key)
-    
-    ''' If no key, returns none '''
-    if key is None or not key:
-      return None 
+    def get(self, key):
+        ''' Returns a value from the cache by key '''
+        if key:
+            return self.cache_data.get(key)
+
+        ''' If no key, returns none '''
+        if key is None or not key:
+            return None 
