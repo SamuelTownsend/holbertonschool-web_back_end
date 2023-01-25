d74f184b23281d820b54f672e12fb4a112224a1e
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-01-25 16:00:05 -0600

    adding code

---
+++ 0x02-personal_data/filtered_logger.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+''' This module contains a funciton named filter_datum '''
+
+import logging
+import mysql.connector
+import os
+import re
+from typing import List
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        ''' Class initialization '''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        ''' Formats log record '''
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            super().format(record),
+                            self.SEPARATOR)
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    ''' Returns a log message '''
+    return re.sub(
+        '|'.join(f"(?<={field}=).*?(?={separator})" for field in fields),
+        redaction,
+        message
+    )
+
+
+def get_logger() -> logging.Logger:
+    ''' This will create a logger '''
+    user_data = logging.getLogger('user_data')
+    user_data.setLevel(logging.INFO)
+    user_data.propagate = False
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(RedactingFormatter(PII_FIELDS))
+    user_data.addHandler(streamHandler)
+    return user_data
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    ''' this function connects to the database '''
+    return mysql.connector.connect(
+        host=os.environ.get('PERSONAL_DATA_DB_HOST'),
+        database=os.environ.get('PERSONAL_DATA_DB_NAME'),
+        user=os.environ.get('PERSONAL_DATA_DB_USERNAME'),
+        password=os.environ.get('PERSONAL_DATA_DB_PASSWORD')
+    )
+
+
+def main():
+    ''' gets rows and displays them in the user table '''
+    db = get_db()
+    log = get_logger()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM users;')
+    for row in cursor:
+        log.warning('; '.join(f'{key}={value}' for key, value in row.items()))
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
