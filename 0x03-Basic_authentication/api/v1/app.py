9f47c5e9d28ec93302f02ad518cd4ed830fd5fd3
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-01-25 23:25:17 -0600

    adding code

--- 0x03-Basic_authentication/api/v1/app.py
+++ 0x03-Basic_authentication/api/v1/app.py
@@ -8,10 +8,32 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
-
+path_list = ['/api/v1/status/',
+             '/api/v1/unauthorized/',
+             '/api/v1/forbidden/']
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+AUTH_TYPE = getenv("AUTH_TYPE")
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+if AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    ''' Error handler for 401 '''
+    return jsonify({'error': 'Unauthorized'}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    ''' Error handler for 403 '''
+    return jsonify({'error': 'Forbidden'}), 403
 
 
 @app.errorhandler(404)
@@ -19,6 +41,19 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.before_request
+def before_request() -> str:
+    ''' before_request handler '''
+    if auth is None:
+        return
+    if not auth.require_auth(request.path, path_list):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
