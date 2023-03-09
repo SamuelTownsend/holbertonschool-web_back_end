116685c60c754a12376be7ca480165854ae9afc9
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-03-09 10:50:31 -0600

    adding second question

---
+++ 0x00-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+// Function will display message and ask for name
+
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin
+  .on('readable', () => {
+    const name = process.stdin.read();
+    if (name) {
+      process.stdout.write(`Your name is: ${name}`);
+    }
+  })
+  .on('end', () => {
+    console.log('This important software is now closing');
+  });
+  
