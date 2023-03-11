86a53c8b78e3030f45b2ddb0672e93d12065b896
SamuelTownsend <91908426+SamuelTownsend@users.noreply.github.com>
2023-03-11 14:09:26 -0600

    Adding initial code and environment

---
+++ 0x02-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,25 @@
+
+// Connects to redis server and logs message to console
+const redis = require('redis');
+const redisCLI = redis.createClient();
+const { promisify } = require('utils');
+
+redisCLI
+	.on('connect', () => {
+		console.log('Redis client connected to the server');
+	})
+	.on('error', (err) => {
+		console.log(`Redis client not connected to the server: ${err}`);
+	});
+
+const setNewSchool = (schoolName, value) => {
+	redisCLI.set(schoolName, value, redis.print);
+};
+
+const displaySchoolValue = async (schoolName) => {
+	console.log(await promisify(redisCLI.get).bind(redisCLI)(schoolName));
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
